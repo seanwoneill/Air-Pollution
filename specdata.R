fe2af4ba3ec3abc64ab70019dd1838e7ec9f5d00
S W ONeill <seanwoneill@gmail.com>
2015-01-14 21:39:09 -0500

    Up 2

--- specdata.R
+++ specdata.R
@@ -1,24 +1,30 @@
 directory <- "C:\\Users\\sean\\Courses\\Coursera R Programming\\Project 1 Data\\specdata"
 pollutantmean <- function(directory, pollutant, id = 1:332) {
+  ## 'directory' is a character vector of length 1 indicating
+  ## the location of the CSV files
+  
+  ## 'pollutant' is a character vector of length 1 indicating
+  ## the name of the pollutant for which we will calculate the
+  ## mean; either "sulfate" or "nitrate".
+  
+  ## 'id' is an integer vector indicating the monitor ID numbers
+  ## to be used
+  
+  ## Return the mean of the pollutant across all monitors list
+  ## in the 'id' vector (ignoring NA values)
   
   ## Set Directory
   setwd(directory)
   
-  pol <- if(substr(pollutant,1,2) == "n"){
-    print(pollutant[1])
-    return "nitrate"
+  if(tolower(substr(pollutant,1,2)) == "n"){
+    ## If Nitrate
+    pol <- 1
   }
-  else{"sulfate" }
-        ## 'directory' is a character vector of length 1 indicating
-        ## the location of the CSV files
+  ## If Sulfate
+  else{pol <- 2 }
+  
+  print(pol)
+  
+  readcsv()
 
-        ## 'pollutant' is a character vector of length 1 indicating
-        ## the name of the pollutant for which we will calculate the
-        ## mean; either "sulfate" or "nitrate".
-
-        ## 'id' is an integer vector indicating the monitor ID numbers
-        ## to be used
-
-        ## Return the mean of the pollutant across all monitors list
-        ## in the 'id' vector (ignoring NA values)
 }
