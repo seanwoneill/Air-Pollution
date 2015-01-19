3c4d4198b3f9e6c892cc0a26f35330b93855d7a5
S W ONeill <seanwoneill@gmail.com>
2015-01-19 08:52:32 -0500

    Up 4

--- specdata.R
+++ specdata.R
@@ -1,38 +1,36 @@
-directory <- "C:\\Users\\sean\\Courses\\Coursera R Programming\\Project 1 Data\\specdata"
-pollutantmean <- function(directory, pollutant, id = 1:332) {
-  ## 'directory' is a character vector of length 1 indicating
-  ## the location of the CSV files
+
+setwd("C:\\Users\\sean\\Courses\\Coursera R Programming\\Project 1 Data\\specdata")
+pollutantmean <- function(directory = getwd(), pollutant = "sulfate", id = 1:332) {
   
-  ## 'pollutant' is a character vector of length 1 indicating
-  ## the name of the pollutant for which we will calculate the
-  ## mean; either "sulfate" or "nitrate".
-  
-  ## 'id' is an integer vector indicating the monitor ID numbers
-  ## to be used
-  
-  ## Return the mean of the pollutant across all monitors list
-  ## in the 'id' vector (ignoring NA values)
-  
-  ## Set Directory
   setwd(directory)
   
-  if(tolower(substr(pollutant,1,2)) == "n"){
+  if(tolower(substr(toString(pollutant),1,2)) == "n"){
     ## If Nitrate
     pol <- 3
   }
   ## If Sulfate
   else{pol <- 2 }
   
-  print(pol)
-  
-  for(i in length(dir(getwd())){
-    if(){
-    currentFile <- dir(getwd())[i]
-    
+  for(i in 1:length(dir(directory))){
+    currentFilePath <- normalizePath(dir(directory)[i])  ##Get name of current file
+    ##Get file name w/o .csv and convert to integer
+    currentFileNum <- as.integer(substr(dir(directory)[i],1,3))
+    if(currentFileNum %in% id){
+      temp <- read.csv(currentFilePath)
+      temp <- temp[pol]
+      temp <- na.omit(temp)
+      if (exists("store")){ ##if not first instance and store is object
+        store <- paste(store,temp,sep="",collapse="")
+        #store <- union(store,temp)
+      }else{ ##if first instance and store not yet object
+        store = temp
+      }
     }
+    rm(temp)
   }
   
-  readFiles <- read.csv(id))
-  mean.default(readfiles,na.rm=TRUE)
-
+  store <- matrix(store,ncol=10)
+  write(store,file = normalizePath("store.txt"))
+  final <- mean(store)
+  return(final)
 }
